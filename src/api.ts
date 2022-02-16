855e9998b32e9784ad54a5023ae1cbc7001008ce
IAmAshokBhat <ashokbhatz@gmail.com>
2022-02-16 13:43:26 +0530

    Added API URL

--- src/api.ts
+++ src/api.ts
@@ -9,47 +9,48 @@ import { fetchWithToken, getToken } from './utils';
 const headers = {
   'X-JWT-Token': getToken()
 };
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const getUserListAPI = () => fetchWithToken('user/list');
 
 export const getUserDetailsAPI = (id: number) =>
-  fetchWithToken(`api/users/${id}`);
+  fetchWithToken(`${API_URL}api/users/${id}`);
 
 export const getAttendanceDetailsAPI = (id: number, from: string, to: string) =>
-  fetchWithToken(`api/attendance/${id}?from=${from}&to=${to}`);
+  fetchWithToken(`${API_URL}api/attendance/${id}?from=${from}&to=${to}`);
 
 export const getLeaveBalanceAPI = (id: number) =>
-  fetchWithToken(`api/leaves/${id}`);
+  fetchWithToken(`${API_URL}api/leaves/${id}`);
 
 export const getEventDetailsAPI = (id: number, from: string, to: string) =>
-  fetchWithToken(`api/events/${id}?from=${from}&to=${to}`);
+  fetchWithToken(`${API_URL}api/events/${id}?from=${from}&to=${to}`);
 
 export const getAllLeavesAPI = (id: number) =>
-  fetchWithToken(`api/all-leaves/${id}`);
+  fetchWithToken(`${API_URL}api/all-leaves/${id}`);
 
 export const applyLeaveAPI = (newLeave: INewLeave) =>
-  fetchWithToken(`api/apply-leave`, {
+  fetchWithToken(`${API_URL}api/apply-leave`, {
     method: 'POST',
     body: JSON.stringify(newLeave)
   });
 
 export const addAttendanceAPI = (newAttendance: INewAttendance) =>
-  fetchWithToken(`api/attendance`, {
+  fetchWithToken(`${API_URL}api/attendance`, {
     method: 'POST',
     body: JSON.stringify(newAttendance)
   });
 
 export const loginAPI = (userInfo: ILoginData) =>
-  fetchWithToken(`login`, {
+  fetchWithToken(`${API_URL}login`, {
     method: 'POST',
     body: JSON.stringify(userInfo)
   });
 
 export const getDesignationListAPI = () =>
-  fetchWithToken(`designation`, { headers: headers });
+  fetchWithToken(`${API_URL}designation`, { headers: headers });
 
 export const registerAPI = (newUser: IRegisterAPI) =>
-  fetchWithToken(`user`, {
+  fetchWithToken(`${API_URL}user`, {
     method: 'POST',
     body: JSON.stringify(newUser)
   });
